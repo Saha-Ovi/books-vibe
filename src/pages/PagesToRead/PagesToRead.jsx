1f62b96186b271e8309103c7a33499b58da236f5
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-08 01:26:05 +0600

    chart customized

--- src/pages/PagesToRead/PagesToRead.jsx
+++ src/pages/PagesToRead/PagesToRead.jsx
@@ -28,7 +28,7 @@ const ChartContent = () => {
   }));
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={600}>
       <BarChart
         data={chartData}
         margin={{ top: 20, right: 30, left: 20, bottom: 60 }}
