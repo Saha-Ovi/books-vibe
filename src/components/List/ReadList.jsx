16f724c06c98a69c3fe45302fee5bbb8491518c1
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-07 16:54:47 +0600

    book section read book and wish functionality done

---
+++ src/components/List/ReadList.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../../context/BookContext';
+import { IoLocationOutline } from 'react-icons/io5';
+import { FaUserFriends } from 'react-icons/fa';
+import { MdOutlineContactPage } from 'react-icons/md';
+
+const ReadList = () => {
+    const { readBook } = useContext(BookContext);
+    return (
+        <div>
+            {
+                readBook.map(book => {
+                    return (
+                        <div className='container mx-auto my-8 shadow-md'>
+                            <div className="grid grid-cols-2  bg-base-100 shadow-sm h-full">
+                                <figure className='w-full flex justify-center items-center h-full'>
+                                    <img
+                                        src={book.image}
+                                        alt="Album"
+                                        className='max-h-[400px] object-contain'
+                                    />
+                                </figure>
+                                <div className="card-body h-full">
+                                    <h2 className="card-title text-3xl font-bold">{book.bookName}</h2>
+                                    <p className='text-xl font-semibold'>By : {book.author}</p>
+                                    <div className='border-y border-gray-400'>
+                                        <p className='text-lg'>{book.category}</p>
+                                    </div>
+                                    <p className='text-sm'>Review: {book.review} </p>
+                                    <div className='flex gap-4 '>
+                                        {
+                                            book.tags.map((tag, index) => <div key={index} className="badge bg-green-100 text-green-600"> # {tag}</div>
+                                            )
+                                        }
+                                        <span className='flex items-center gap-1 justify-center text-sm'><IoLocationOutline /> Year of Publishing : {book.yearOfPublishing} </span>
+                                    </div>
+                                    <div className='flex items-center gap-2 justify-center'>
+                                        <p className='flex items-center gap-1 '><FaUserFriends /> Publisher :  {book.publisher}</p>
+                                        <p className='flex items-center gap-1'> <MdOutlineContactPage /> Page : {book.totalPages} </p>
+                                    </div>
+                                    <div className='border-t border-gray-200 p-4 flex gap-2'>
+                                        <p className='badge bg-blue-100 text-blue-500 p-4 rounded-full'>Category :{book.category}</p>
+                                        <p className='badge bg-orange-100 text-orange-500 p-4 rounded-full'>Rating : {book.rating}</p>
+                                        <p className='badge bg-green-500 text-white p-4 rounded-full'><a href="/">View Details</a></p>
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+                )
+            }
+        </div>
+    );
+};
+
+export default ReadList;
--- src/pages/BooksPage/BooksPage.jsx
+++ src/pages/BooksPage/BooksPage.jsx
@@ -1,12 +1,28 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../../context/BookContext';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import ReadList from '../../components/List/ReadList';
+import WishList from '../../components/List/WishList';
 
 const BooksPage = () => {
-    const {readBook, wishList }=useContext(BookContext);
+    // const { readBook, wishList } = useContext(BookContext);
     return (
         <div className='container mx-auto'>
-            <h2>ReadBook : {readBook.length}</h2>
-            <h2>Wish list : {wishList.length}</h2>
+          
+            <Tabs>
+                <TabList>
+                    <Tab>Read Books</Tab>
+                    <Tab>WishList Books</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <ReadList></ReadList>
+                </TabPanel>
+                <TabPanel>
+                    <WishList></WishList>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
